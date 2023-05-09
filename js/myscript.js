c21b1ed798ae0bac791ce68d8ebcb02757c9e143
gabrielemafrica <gabriele.ga@live.it>
2023-05-09 22:17:22 +0300

    con livelli di difficolta

--- js/myscript.js
+++ js/myscript.js
@@ -1,59 +1,54 @@
 
 // costruisco una griglia con square numerati e cliccabili
 //prendo un container
-const container = document.getElementById("container");
+const container = document.getElementById("grid");
+
+//prendo il livello
+const level = parseInt(document.getElementById("level").value);
+console.log(level);
 //prendo il bottone
 
 const play = document.getElementById("play");
 
 //popolo array di numeri
 
-const numeriArray = arrayNumeri(1, 100);
+const easy = arrayNumeri(1, 100);
+console.log(easy);
+const normal = arrayNumeri(1, 81);
+console.log(normal);
+const hard = arrayNumeri(1, 49);
+console.log(hard);
 // console.log(numeriArray);
 
 //evento click
-
 play.addEventListener(
     "click",
-
-    function () {
-    //creo il contenitore
-    const containerGrid = createElemento("div", "container-grid");
-    //creo la griglia
-    for (let i = 0; i < numeriArray.length - 1; i++) {
-        //creo il quadrato
-        const square = createElemento("div", "square");
-        //metto i numeri in uno span
-        //crea span
-        const numero = createElemento("span", "number");
-        //metto il numero
-        numero.append(numeriArray[i]);
-        //metto span nel square
-        square.append(numero);
-
-        //al click lo square si colora
-        square.addEventListener(
-            'click',
-            function () {
-                if (this.classList.contains('selected')) {
-                    this.classList.remove('selected');
-                    console.log(`Hai DESELEZIONATO il numero ${numeriArray[i]}`);
-                }else{
-                    this.classList.add('selected');
-                    console.log(`Hai SELEZIONATO il numero ${numeriArray[i]}`);
-                }
-                
+    function(){
+        if (level === 1){
+            generaGrigliaConLivello(easy);
+            //aggiungo la classe difficolta
+            const quadrati = document.querySelectorAll("div.square");
+            for (let i = 0; i < quadrati.length; i++) {
+                quadrati[i].classList.add("easy");
             }
-        )
-
-        //metto square nel container
-        containerGrid.append(square);
-        //metto il container nel container della pagina
-        container.append(containerGrid);
-    }
-    
+        }else if (level === 2) {
+            generaGrigliaConLivello(normal);
+            //aggiungo la classe difficolta
+            const quadrati = document.querySelectorAll("div.square");
+            for (let i = 0; i < quadrati.length; i++) {
+                quadrati[i].classList.add("normal");
+            }
+        } else if (level === 3) {
+            generaGrigliaConLivello(hard);
+            //aggiungo la classe difficolta
+            const quadrati = document.querySelectorAll("div.square");
+            for (let i = 0; i < quadrati.length; i++) {
+                quadrati[i].classList.add("hard");
+            }       
+        }
     }
 )
+    
 
 
 
@@ -67,10 +62,49 @@ function createElemento(tagType, classToAdd) {
 //creo un array ordinato con inizio e fine
 function arrayNumeri(numIniziale, numFinale){
     const arr = [];
-    for (let i = numIniziale; i <= numFinale + 1; i++){
+    for (let i = numIniziale; i <= numFinale; i++){
         arr.push(i);
     }
     return arr;    
+}
+//finzione che genera la griglia a seconda del livello
+function generaGrigliaConLivello(livelloDifficolta) {
+    //creo il contenitore
+    const containerGrid = createElemento("div", "container-grid");
+    //creo la griglia
+  
+    for (let i = 0; i < livelloDifficolta.length; i++) {
+        //creo il quadrato
+        const square = createElemento("div", "square");
+
+        //metto i numeri in uno span
+        //crea span
+        const numero = createElemento("span", "number");
+        //metto il numero
+        numero.append(livelloDifficolta[i]);
+        //metto span nel square
+        square.append(numero);
+        
+        //al click lo square si colora
+        square.addEventListener(
+            'click',
+            function () {
+                if (this.classList.contains('selected')) {
+                    this.classList.remove('selected');
+                    console.log(`Hai DESELEZIONATO il numero ${livelloDifficolta[i]}`);
+                }else{
+                    this.classList.add('selected');
+                    console.log(`Hai SELEZIONATO il numero ${livelloDifficolta[i]}`);
+                }
+                
+            }
+        )
+            
+            //metto square nel container
+            containerGrid.append(square);
+            //metto il container nel container della pagina
+            container.append(containerGrid);
+    }
 }
 
 
